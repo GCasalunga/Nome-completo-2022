be64fd0acb2ee7370129c39dec4ff7f8cc67f6d8
Giovanna <giovannacasalunga@gmail.com>
2022-03-13 15:10:31 -0300

    Tarefa finalizada

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿string nome, sobrenome;
+Console.Write("Digite seu primeiro nome:   ");
+nome = Console.ReadLine()!;
+Console.Write("Digite seu segundo nome:   ");
+sobrenome = Console.ReadLine()!;
+string maiúscula = sobrenome.ToUpper();
+Console.WriteLine($"Nome Completo: {nome}, {sobrenome}");
+Console.WriteLine($"Nome de catálogo: {maiúscula}, {nome}");
+
